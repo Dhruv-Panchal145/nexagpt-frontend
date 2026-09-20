0608deb317852d77cabcdd7156a81867c5413242
Dhruv Panchal <dhruvpanchal1345@gmail.com>
2026-09-20 12:13:55 +0530

    chnage in gpt logo

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,7 +2,7 @@ import "./Sidebar.css";
 import { useContext, useEffect } from "react";
 import { MyContext } from "./MyContext";
 import { v1 as uuidv1 } from "uuid";
-import logoSvg from "./assets/logo.svg"; 
+import logoSvg from "./assets/logo.png"; 
 
 function Sidebar() {
   const { 
